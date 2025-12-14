69ead1dc3c4f2e8f7a73cdadfa56ff278626205f
Waffle <csshin9928@gmail.com>
2025-12-14 19:55:22 +0900

    feat: implement system settings for logging

---
+++ Farmomatica.Core/Utils/Setting/SystemSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace Farmomatica.Core.Utils.Setting;
+
+internal static class SystemPaths
+{
+    private static readonly string _appDataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), ".farmomatica");
+
+    public static readonly string LogFolderPath = Path.Combine(_appDataPath, "logs");
+    public const string LogFileName = "latest.log";
+}
+
+internal static class TimeoutSettings
+{
+    public static readonly TimeSpan ArchiveTimeout = TimeSpan.FromSeconds(60);
+}
