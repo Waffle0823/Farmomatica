1b652f793be090de3752e838efd46284c5751dee
Waffle <csshin9928@gmail.com>
2025-12-19 03:33:32 +0900

    feat: make abstract class for system info providers

---
+++ Farmomatica.Core/Utils/SystemInfo/SystemInfoCollector.cs
@@ -0,0 +1,10 @@
+﻿namespace Farmomatica.Core.Utils.SystemInfo;
+
+public abstract class SystemInfoCollector
+{
+    public string GetCpuName() => GetCpuNameInternal() ?? "Unknown CPU";
+    public ulong GetRamBytes() => GetRamBytesInternal() ?? 0UL;
+
+    protected abstract string GetCpuNameInternal();
+    protected abstract ulong? GetRamBytesInternal();
+}
