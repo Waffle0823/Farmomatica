43df6521e6e527c9a608f68dc342fa1fe93f15d6
Waffle <csshin9928@gmail.com>
2025-12-30 20:49:35 +0900

    refactor: handle exceptions at logger class

--- Farmomatica.Core/Common/Logging/Logger.cs
+++ Farmomatica.Core/Common/Logging/Logger.cs
@@ -9,7 +9,6 @@ public sealed class Logger : IDisposable
     private readonly ILogSink _sink;
 
     private readonly Channel<LogEntry> _logChannel = Channel.CreateUnbounded<LogEntry>();
-    private readonly Channel<CrashEntry> _crashChannel = Channel.CreateUnbounded<CrashEntry>();
 
     private readonly Task _processTask;
 
@@ -17,7 +16,7 @@ public sealed class Logger : IDisposable
     {
         _sink = sink ?? throw new ArgumentNullException(nameof(sink));
 
-        _processTask = Task.WhenAll(ProcessLog(), ProcessCrash());
+        _processTask = Task.Run(ProcessLog);
     }
 
     public void Log(in LogEntry entry)
@@ -27,13 +26,17 @@ public sealed class Logger : IDisposable
 
     public void Crash(in CrashEntry entry)
     {
-        _crashChannel.Writer.TryWrite(entry);
+        if (!_sink.TryWriteCrash(entry))
+        {
+#if DEBUG
+            Console.WriteLine("Error while writing crash report");
+#endif
+        }
     }
 
     public void Dispose()
     {
         _logChannel.Writer.Complete();
-        _crashChannel.Writer.Complete();
 
         _processTask.Wait();
 
@@ -43,16 +46,14 @@ public sealed class Logger : IDisposable
     private async Task ProcessLog()
     {
         await foreach (LogEntry entry in _logChannel.Reader.ReadAllAsync())
-        {
-            _sink.TryWrite(entry);
-        }
-    }
 
-    private async Task ProcessCrash()
-    {
-        await foreach (CrashEntry entry in _crashChannel.Reader.ReadAllAsync())
         {
-            _sink.TryWriteCrash(entry);
+            if (!_sink.TryWrite(entry))
+            {
+#if DEBUG
+                Console.WriteLine("Error while writing log");
+#endif
+            }
         }
     }
 }
