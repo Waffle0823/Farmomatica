44797b0d00e277c072c9716dc2948eb205d28da5
Waffle <csshin9928@gmail.com>
2025-12-31 03:12:29 +0900

    refactor: correct paramater order

--- Farmomatica.Core/Common/Logging/Log.cs
+++ Farmomatica.Core/Common/Logging/Log.cs
@@ -13,15 +13,15 @@ public static class Log
         _instance = logger;
     }
 
-    public static void Debug(string message, string category) => _instance?.Log(new LogEntry(LogLevel.Debug, message, category));
+    public static void Debug(string category, string message) => _instance?.Log(new LogEntry(LogLevel.Debug, message, category));
 
-    public static void Info(string message, string category) => _instance?.Log(new LogEntry(LogLevel.Info, message, category));
+    public static void Info(string category, string message) => _instance?.Log(new LogEntry(LogLevel.Info, message, category));
 
-    public static void Warn(string message, string category) => _instance?.Log(new LogEntry(LogLevel.Warn, message, category));
+    public static void Warn(string category, string message) => _instance?.Log(new LogEntry(LogLevel.Warn, message, category));
 
-    public static void Error(string message, string category) => _instance?.Log(new LogEntry(LogLevel.Error, message, category));
+    public static void Error(string category, string message) => _instance?.Log(new LogEntry(LogLevel.Error, message, category));
 
-    public static void Critical(string message, string category) => _instance?.Log(new LogEntry(LogLevel.Critical, message, category));
+    public static void Critical(string category, string message) => _instance?.Log(new LogEntry(LogLevel.Critical, message, category));
 
     public static void Crash(Exception ex) => _instance?.Crash(new CrashEntry(ex));
 }
