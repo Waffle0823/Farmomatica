f0b2d2306f2efb534dca3baf5ee7cc905db8bcfc
Waffle <csshin9928@gmail.com>
2026-01-03 02:06:40 +0900

    feat: initialize logger and pass platform runtime class

--- Farmomatica.DesktopGL/Program.cs
+++ Farmomatica.DesktopGL/Program.cs
@@ -1,2 +1,11 @@
-﻿using var game = new Farmomatica.Core.Game1();
+﻿using Farmomatica.Core.Common.Logging;
+using Farmomatica.DesktopGL.Platform;
+using Farmomatica.DesktopGL.Settings;
+using Farmomatica.DesktopGL.Sinks;
+
+using Logger logger = new(new LogSink(SystemPaths.LogFilePath, SystemPaths.LogArchiveFolderPath, SystemPaths.CrashFolderPath));
+
+Log.Initialize(logger);
+
+using var game = new Farmomatica.Core.Game1(new PlatformRuntime());
 game.Run();
