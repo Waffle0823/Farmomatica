287be1f617b7735f98812741e12f9b3907d392ec
Waffle0823 <csshin9928@gmail.com>
2026-01-23 16:26:39 +0900

    feat: add basic raylib window with hello world text

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
-#include <iostream>
 #include <raylib.h>
 
-int main(int, char**){
-    std::cout << "Hello, from Farmomatica!\n";
+int main(int, char **) {
+  constexpr int windowWidth = 800;
+  constexpr int windowHeight = 600;
+
+  InitWindow(windowWidth, windowHeight, "Farmomatica");
+
+  SetTargetFPS(60);
+
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    DrawText("Hello raylib!", 190, 200, 20, DARKGRAY);
+    EndDrawing();
+  }
+
+  CloseWindow();
+  return 0;
 }
