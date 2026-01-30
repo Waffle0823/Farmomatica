ef9e35d72421f136567f94703b837c9b7f5415cd
ChanSoo Shin <csshin9928@gmail.com>
2026-01-31 00:32:11 +0900

    refactor: remove LogSinker interface

--- src/utils/logging/ILogSinker.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-
-class ILogSinker {
-public:
-  virtual ~ILogSinker() = default;
-  virtual void Log(const std::string &category, const std::string &message) = 0;
-};
