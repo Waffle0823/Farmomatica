3eb7a186ea240ec4a3edd27226bb7d9546c790b1
Waffle0823 <csshin9928@gmail.com>
2026-03-22 18:30:44 +0900

    feat: add GameData class for managing application resource paths

---
+++ include/common/data/game_data.h
@@ -0,0 +1,9 @@
+#include <filesystem>
+
+class GameData {
+public:
+  static std::filesystem::path GetTexturesPath();
+  static std::filesystem::path GetShadersPath();
+  static std::filesystem::path GetSavesPath();
+  static std::filesystem::path GetConfigPath();
+};
