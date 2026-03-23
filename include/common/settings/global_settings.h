4efc81f5d0d23af82d52c31b1c0396a04dfa47f8
Waffle0823 <csshin9928@gmail.com>
2026-03-24 00:01:00 +0900

    refactor: organize global settings into Application and Assets namespaces and add null texture dimensions

--- include/common/settings/global_settings.h
+++ include/common/settings/global_settings.h
@@ -6,9 +6,19 @@
 namespace farmomatica {
 namespace Settings {
 namespace Global {
+
+namespace Application {
 inline constexpr std::string_view NAME = "Farmomatica";
-inline constexpr std::array<std::string_view, 1> SUPPORTED_TEXTURE_EXTENSIONS = {
+} // namespace Application
+
+namespace Assets {
+namespace Textures {
+inline constexpr std::array<std::string_view, 1> SUPPORTED_EXTENSIONS = {
     ".png"};
+inline constexpr int NULL_TEXTURE_WIDTH = 32;
+inline constexpr int NULL_TEXTURE_HEIGHT = 32;
+} // namespace Textures
+} // namespace Assets
 } // namespace Global
 } // namespace Settings
 } // namespace farmomatica
--- include/common/utils/extension_utils.h
+++ include/common/utils/extension_utils.h
@@ -9,10 +9,10 @@
 namespace farmomatica {
 namespace Utils {
 inline constexpr bool isTextureExtensionSupported(std::string_view extension) {
-  return std::find(Settings::Global::SUPPORTED_TEXTURE_EXTENSIONS.begin(),
-                   Settings::Global::SUPPORTED_TEXTURE_EXTENSIONS.end(),
+  return std::find(Settings::Global::Assets::Textures::SUPPORTED_EXTENSIONS.begin(),
+                   Settings::Global::Assets::Textures::SUPPORTED_EXTENSIONS.end(),
                    extension) !=
-         Settings::Global::SUPPORTED_TEXTURE_EXTENSIONS.end();
+         Settings::Global::Assets::Textures::SUPPORTED_EXTENSIONS.end();
 }
 } // namespace Utils
 } // namespace farmomatica
