c96484f71e311e65f881880b908ddfac7df82006
Waffle0823 <csshin9928@gmail.com>
2026-03-24 00:22:39 +0900

    refactor: add Doxygen documentation to GameData resource path methods

--- include/common/data/game_data.h
+++ include/common/data/game_data.h
@@ -6,10 +6,39 @@ namespace farmomatica {
 
 class GameData {
 public:
+  /**
+   * @brief Gets the path to the textures directory.
+   * @return The filesystem path to the textures directory.
+   * @throws std::filesystem::filesystem_error If the directory cannot be created or accessed.
+   */
   static std::filesystem::path GetTexturesPath();
+
+  /**
+   * @brief Gets the path to the shaders directory.
+   * @return The filesystem path to the shaders directory.
+   * @throws std::filesystem::filesystem_error If the directory cannot be created or accessed.
+   */
   static std::filesystem::path GetShadersPath();
+
+  /**
+   * @brief Gets the path to the logs directory.
+   * @return The filesystem path to the logs directory.
+   * @throws std::filesystem::filesystem_error If the directory cannot be created or accessed.
+   */
   static std::filesystem::path GetLogsPath();
+
+  /**
+   * @brief Gets the path to the saves directory.
+   * @return The filesystem path to the saves directory.
+   * @throws std::filesystem::filesystem_error If the directory cannot be created or accessed.
+   */
   static std::filesystem::path GetSavesPath();
+
+  /**
+   * @brief Gets the path to the config directory.
+   * @return The filesystem path to the config directory.
+   * @throws std::filesystem::filesystem_error If the directory cannot be created or accessed.
+   */
   static std::filesystem::path GetConfigPath();
 };
 
